55fc1a82f58cdeda363e796c481e455af5900dcd
gitsibi <159125779+gitsibi@users.noreply.github.com>
2024-02-29 15:53:33 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -26,7 +26,16 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    // Create players using for loop
+    for (let i = 0; i < players.length; i++) {
+        let player = {
+            name: players[i],
+            strength: getRandomStrength(),
+            image: `images/super-${i + 1}.png`,
+            type: (i % 2 === 0) ? 'hero' : 'villain' // Alternating between hero and villain
+        };
+        detailedPlayers.push(player);
+    }
     return detailedPlayers;
 }
 
@@ -34,7 +43,8 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
-}
+    return Math.ceil(Math.random() * 100);
+};
 
 const buildPlayers = (players, type) => {
     let fragment = '';
@@ -42,9 +52,31 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+    // Loop through players and accumulate HTML template
+    for (let player of players) {
+        if (player.type === type) {
+            fragment += `
+            <div class="player">
+           <img src="${player.image}" alt="${player.name}">
+           <div class="name">${player.name}</div>
+           <div class="strength">${player.strength}</div>
+           </div> `;
+        }
+    }
     return fragment;
 }
+//  <div class="player">
+//    <img src="${players[i].image}">
+//    <div class="name">${players[i].name}</div>
+//    <div class="strength">${players[i].strength}</div>
+// </div> 
+
+//   '<div class="player">
+//  <img src="${player.image}" alt="${player.name}">
+//  <div class="name">${player.name}</div>
+//  <div class="strength">${player.strength}</div>
+//  </div>' 
+
 // Display players in HTML
 const viewPlayers = (players) => {
 
